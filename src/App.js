4d8e076f36065259f8082d1db3e4d2cca72c020a
Jonathan Haber <jryanhaber@gmail.com>
2022-07-16 14:17:45 -0600

    create api and populate state with dummy friends data

--- src/App.js
+++ src/App.js
@@ -12,24 +12,45 @@ import React, { Component } from 'react';
 
 // use case example, you create an even on environmentalism, you can invite your friends for free but facebook will often limit how many people you can invite, so being able to more easily target the right people can radically expand your reach.
 
+// order of next actions
+// pull data from an api to get the contents of these people
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { name: 'Jonathan', key: '96872367' },
-        { name: 'David', key: '968d723d67' },
-        { name: 'Jason', key: '96d872d367' },
-        { name: 'Marc', key: 'dasdfdsaf' },
-        { name: 'Katya', key: '4asd' }
-      ]
+      friends: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      // console.log(response);
+      // the response from the fetch is this response cors object with a readable stream as a body, which wont exactly show in the console.
+      // console.log(response.body);
+      // but when converted to json() it changes to this promise object which has a result, and the result is the array.  Thats the array we want to use initially to set our app state as the users object.
+      // console.log(response.json());
+      // so by passing the response to a new function which then returns the response.json() ( conversion ) we in essence transform the property that we are passing through this function, like taking the response and passing to a function sort() which returns the sorted array.
+      // so this following statement says, then after the returned function completes, take what is returned by it ( response.json ), and pass that as a property called users and console log it
+
+      .then((friends) =>
+        this.setState(
+          () => {
+            return { friends: friends };
+          },
+
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <div key={monster.id}>{monster.name}</div>;
+        {this.state.friends.map((friend) => {
+          return <div key={friend.id}>{friend.name}</div>;
         })}
       </div>
     );
